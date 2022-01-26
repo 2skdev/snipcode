26a6ed726f06db4c7f9fbc4de1a1ea8042407a9a
choro0121 <t.chay9271@gmail.com>
2022-01-26 17:54:43 +0900

    add register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,57 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Textarea from "react-textarea-autosize";
+import Logo from "@/assets/logo.svg";
+import Image from "@/assets/soco-st_12260_paint.svg";
+
+const Register = (): JSX.Element => {
+  useEffect(() => {
+    document.body.classList.add("bg-gray-100");
+  });
+
+  const router = useRouter();
+  const session = useSession();
+
+  const signup = () => {
+    // todo: signup
+    router.push("/");
+  };
+
+  return (
+    <>
+      <main className="container md:max-w-screen-md mx-auto flex flex-col items-center pt-10">
+        <Logo />
+        <Image className="mt-10" />
+
+        <div className="mt-5">
+          <div className="ml-1 text-gray-500">ユーザー名</div>
+          <input
+            className="w-96 mt-1 px-4 py-2 rounded border border-transparent focus:border-orange-500 focus:outline-none text-gray-700"
+            placeholder="@"
+          />
+        </div>
+
+        <div className="mt-5">
+          <div className="ml-1 text-gray-500">自己紹介</div>
+          <Textarea
+            className="w-96 mt-1 px-4 py-2 rounded border border-transparent focus:border-orange-500 focus:outline-none text-gray-700 resize-none overflow-y-auto"
+            placeholder="bio"
+          />
+        </div>
+
+        <button className="btn-primary mt-5 px-3 py-1" onClick={signup}>
+          登録する
+        </button>
+        <button
+          className="btn-text-secondary mt-2 px-3 py-1 underline"
+          onClick={() => router.back()}
+        >
+          キャンセル
+        </button>
+      </main>
+    </>
+  );
+};
+
+export default Register;
