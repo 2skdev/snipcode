b412e34d9fd85a839527c0363c32f4c81fe584f0
choro0121 <t.chay9271@gmail.com>
2022-01-27 17:43:36 +0900

    add random postId function

--- src/pages/api/v1/posts/index.ts
+++ src/pages/api/v1/posts/index.ts
@@ -1,10 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import crypto from "crypto";
+
+const random = (length: number): string => {
+  const rand = crypto.randomBytes(length >> 1).toString("hex");
+  return rand;
+};
 
 const PostsHandler = (req: NextApiRequest, res: NextApiResponse): void => {
   if (req.method == "GET") {
     res.status(200).json({});
   } else if (req.method == "POST") {
-    console.log(req.body);
+    const postId = random(16);
+    console.log(postId);
     res.status(200).json({});
   } else {
     res.status(405).json({});
