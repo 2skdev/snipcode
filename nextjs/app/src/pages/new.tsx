c9fc8d79042427d3cb08cbd377f113b8e55455d6
choro0121 <t.chay9271@gmail.com>
2022-02-16 13:49:29 +0000

    add searchable lang select

--- nextjs/app/src/pages/new.tsx
+++ nextjs/app/src/pages/new.tsx
@@ -1,11 +1,116 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useEffect, useRef, useState } from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { AiFillCaretDown } from "react-icons/ai";
 import Textarea from "react-textarea-autosize";
 import { SigninProtect } from "@/contexts/auth";
 import { createPost } from "@/utils/api";
 import { langList } from "@/lib/shiki";
 import { CreatePostRequest } from "@/types/request";
+
+const Select = ({
+  className,
+  options,
+  value,
+  onChange,
+}: {
+  className?: string;
+  options: string[];
+  value: string;
+  onChange: (value: string) => void;
+}): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const input = useRef<HTMLInputElement>(null);
+
+  const select = (selected: string) => {
+    onChange(selected);
+    setOpen(false);
+  };
+
+  const list = options.filter((opt) => ~opt.indexOf(search));
+
+  const next = () => {
+    const index = list.indexOf(value);
+    if (~index) {
+      if (index < list.length - 1) {
+        onChange(list[index + 1]);
+      }
+    } else {
+      onChange(list[0]);
+    }
+  };
+  const prev = () => {
+    const index = list.indexOf(value);
+    if (~index) {
+      if (index > 0) {
+        onChange(list[index - 1]);
+      }
+    } else {
+      onChange(list[0]);
+    }
+  };
+
+  return (
+    <>
+      <div className={"flex flex-col " + className}>
+        <div className="relative flex items-center w-40">
+          <input
+            className={
+              "z-0 px-2 py-1 w-40 focus:outline-none rounded text-gray-600 bg-gray-100 " +
+              (open ? "" : "cursor-pointer")
+            }
+            ref={input}
+            onBlur={() => setOpen(false)}
+            onFocus={() => setOpen(true)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && ~list.indexOf(value)) {
+                setOpen(false);
+                input.current?.blur();
+              } else if (e.key === "ArrowDown") {
+                next();
+              } else if (e.key === "ArrowUp") {
+                prev();
+              }
+            }}
+            value={open ? search : value}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <div
+            className="absolute right-0 mr-2 text-gray-600 cursor-pointer"
+            onMouseDown={() => setOpen(true)}
+          >
+            {!open && <AiFillCaretDown size={14} />}
+          </div>
+        </div>
+
+        {open && (
+          <div className="absolute flex flex-col w-40 max-h-60 overflow-y-scroll mt-10 border rounded">
+            {list.map((opt, index) => (
+              <button
+                className={[
+                  "px-2 py-1 text-left",
+                  index == 0
+                    ? "rounded-t"
+                    : index == list.length - 1
+                    ? "rounded-b"
+                    : "",
+                  opt === value
+                    ? "text-white bg-orange-400 hover:bg-orange-300"
+                    : "text-gray-600 bg-white hover:bg-gray-100",
+                ].join(" ")}
+                key={index}
+                onMouseDown={() => select(opt)}
+              >
+                {opt}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 const New = (): JSX.Element => {
   useEffect(() => {
@@ -16,7 +121,7 @@ const New = (): JSX.Element => {
 
   const [input, setInput] = useState<CreatePostRequest>({
     title: "",
-    language: langList[0],
+    language: "",
     code: "",
   });
 
@@ -37,7 +142,7 @@ const New = (): JSX.Element => {
                 router.back();
               }}
             >
-              <AiOutlineArrowLeft size={24} />
+              <FiArrowLeft size={20} />
             </button>
 
             <div>
@@ -69,17 +174,12 @@ const New = (): JSX.Element => {
               <div className="mb-1 text-lg font-bold ">## Snippet</div>
               <div className="flex">
                 <span className="mt-1">```</span>
-                <select
-                  className="pl-1 ml-1 rounded focus:outline-none bg-gray-100"
+                <Select
+                  className="pl-1"
+                  options={langList}
                   value={input.language}
-                  onChange={(e) =>
-                    setInput({ ...input, language: e.target.value })
-                  }
-                >
-                  {langList.map((lang, index) => (
-                    <option key={index}>{lang}</option>
-                  ))}
-                </select>
+                  onChange={(value) => setInput({ ...input, language: value })}
+                />
               </div>
               <Textarea
                 className="resize-none focus:outline-none overflow-hidden w-full mt-2 p-2 rounded bg-gray-100"
